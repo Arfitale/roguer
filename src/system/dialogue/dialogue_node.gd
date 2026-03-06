ba49aeec610eeeceef991ce7f3ef06fcb99822db
Lavazator <predatorofforsaken@gmail.com>
2026-03-07 06:42:43 +0700

    feat(dialogue): implement dialogue choices and update dialogue system

--- src/system/dialogue/dialogue_node.gd
+++ src/system/dialogue/dialogue_node.gd
@@ -1,6 +1,7 @@
 class_name DialogueNode
 extends RefCounted
 
-var speaker: String;
-var text: String;
-var next: String;
+var speaker: String
+var text: String
+var next: String
+var choices: Array
